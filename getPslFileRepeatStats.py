bb315763b287364c2f97e5d169a31d5f68127e02
Brian Lin <me@brianl.in>
2013-06-03 18:09:26 -0700

    merge script added

--- getPslFileRepeatStats.py
+++ getPslFileRepeatStats.py
@@ -2,6 +2,7 @@
 import sys
 from classes import PierRepeat
 from Bio import SeqIO
+import TupleMergeGenerator
 
 def checkAndReturnArguments():
 	if (len(sys.argv) != 2):
@@ -20,7 +21,7 @@ class RepeatStats(object):
 	def __init__(self):
 		self.BpsByRepeatClassifs = {}
 
-	def addRepeatCopy(self, repeat, blockSize): # repeat is of type PierRepeat
+	def addRepeatCopy(self, repeat, repeatLength): # repeat is of type PierRepeat
 		if repeat.CLASS in self.BpsByRepeatClassifs:
 			self.BpsByRepeatClassifs[repeat.CLASS] += blockSize
 		else: 
@@ -53,6 +54,60 @@ def getSeqIdAndAnnotation(header):
 	annotation = header.split('\t')[1]
 	return (seqID, annotation)
 
+class RedundancyChecker(object):
+	def __init__(self):
+		self.targetSeqs = []
+
+	def addTargetSeq(self, seqID, newSubseq):
+		if seqID in self.targetSeqs:
+			self.targetSeqs[seqID].checkAndAddSubseq(seqID, subseq)
+		else:
+			self.addNewTargetSeq(seqID, subseq)
+
+	def addNewTargetSeq(self, seqID, newSubseq):
+		newTargetSeq = TargetSequenceSubseqs(seqID)
+		newTargetSeq.
+		targetSeqs.append(newTargetSeq)
+
+class TargetSequenceSubseqs(object):
+	def __init__(self, seqID):
+		self.seqID = seqID
+		self.subseqs = []
+	
+	def __eq__(self, other):
+		return self.seqID == other.seqID
+
+	NO_OVERLAP = 0
+	FULL_OVERLAP = 1
+	OVERLAPLEFT = 10
+	OVERLAPRIGHT = 100
+
+	def checkAndAddSubseq(self, seqID, newSubseq):
+		overlapCode = NO_OVERLAP
+		newSubseqStart = newSubseq[0]
+		newSubseqEnd = newSubseq[1]
+		for existingSubseq in self.subseqs[seqID]:
+			existingSubseqStart = existingSubseq[0]
+			existingSubseqEnd = existingSubseq[1]
+			if (newSubseqEnd < existingSubseqEnd) and (newSubseqStart > existingSubseqEnd):
+				return
+			if (newSubseqStart < existingSubseqStart) and (newSubseqEnd > existingSubseqEnd):
+
+			if (newSubseqStart > existingSubseqEnd) or (newSubseqEnd < existingSubseqStart):
+				continue
+			if (newSubseqStart > existingSubseqStart) and (newSubseqEnd > existingSubseqEnd):
+				rightOverlapSeq = existingSubseq
+				overlapCode += OVERLAPRIGHT
+				continue
+			if (newSubseqStart < existingSubseqStart) and (newSubseqEnd < existingSubseqEnd):
+				leftOverlapSeq = existingSubseq
+				overlapCode += OVERLAPLEFT
+				continue
+		if overlapCode == NO_OVERLAP:
+			self.subseqs[seqID].append((newSubseqStart, newSubseqEnd))
+		elif overlapCode == OVERLAPLEFT:
+			self.subseqs[seqID].remove()
+
 if __name__ == '__main__':
 	filename = checkAndReturnArguments()
 	psl_file = open(filename)
@@ -65,6 +120,7 @@ if __name__ == '__main__':
 		seqAnnotation = annotationDict[seqName]
 		currentHit = PierRepeat(seqAnnotation)
 		blockSizes = convertCSVsToIntList(tabs[18]) # tabs[18] is a CSV string
+		filteredBlockSizes = removeRedundancyFrom(blockSizes)
 		for blockSize in blockSizes:
 			stats.addRepeatCopy(currentHit, blockSize)
 		if timer % 400000 == 0:
