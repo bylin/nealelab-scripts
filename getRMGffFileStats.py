93ec5c0c61488def703603d3258246810b584528
Brian Lin <me@brianl.in>
2013-06-17 10:00:13 -0700

    add functionality for pier repeats

--- getRMGffFileStats.py
+++ getRMGffFileStats.py
@@ -13,23 +13,24 @@ def main():
 def getFileList():
 	args = parseArgs()
 	if args.file:
-		return [args.input]
+		return [args.file]
 	elif args.directory:
-		fileList = subprocess.check_output('ls' + args.input).split('\n')
+		fileList = subprocess.check_output('ls' + args.directory).split('\n')
 		return fileList[:-1]
 	else: exit("Could not get file list")
 	
 def parseArgs():
 	argParser = argparse.ArgumentParser(description='Get repeat element stats from RepeatMasker GFF output. Assume annotations include a mixture of Wicker annotations, Repbase annotations, and custom annotations.')
-	group = parser.add_mutually_exclusive_group()
+	group = argParser.add_mutually_exclusive_group(required=True)
 	group.add_argument('-f', '--file', help='Input file')
 	group.add_argument('-d', '--directory', help='Input directory')
-	group.add_argument('input', help='Input file/directory name')
 	return argParser.parse_args()
 	
 
 def addStatsFromFile(stats, fileHandle):
 	myfile = open(fileHandle)
+	# need to skip first 3 lines of GFF files
+	for x in range(0,3): myfile.readline()
 	for line in myfile:
 		stats += parseLineIntoRepeatTuple(line)
 
@@ -38,24 +39,17 @@ def parseLineIntoRepeatTuple(line):
 	repeatName = parsedLine[0][:-1]
 	blockSize = int(parsedLine[2]) - int(parsedLine[1]) + 1
 	repeat = buildRepeatFromName(repeatName)
-	(repeat, blockSize) = trivialParse
+	return (repeat, blockSize)
 
 # check if re.match is faster than array matching
 def buildRepeatFromName(repeatName):
-	if repeatName[0:2] == 'Pt': # either Wicker or custom
-		if repeatName[5] == '_':
-			repeat = classes.WickerRepeat(repeatName[2:5], repeatName)
-		elif repeatName[7] == '_':
-			repeat = classes.WickerRepeat('Uncategorized', repeatName)
-#		elif repeatName[2:11] == 'Potential':
-#			repeat = classes.Wi
-		else:
-			if repeatName in ['PtOuachita', 'PtBastrop', 'PtOzark', 'PtAppalachian', 'PtAngelina', 'PtTalladega']:
-				repeat = classes.WickerRepeat('RLG', repeatName)
-			elif repeatName in ['PtCumberland', 'PtPineywoods', 'PtConagree']:
-				repeat = classes.WickerRepeat('RLC', repeatName)
-			elif repeatName == 'PtPiedmont':
-				repeat = classes.WickerREepat('RLX', repeatName)
-#	else: # Repbase annotation
-#		repeat = classes.PierRepeat(repeatName)
+	if repeatName[0:2] == 'Pt': # either REPET/Wicker, or custom
+		code = repeatName[2:].split('_')[0]
+			repeat = classes.WickerRepeat(code)
+	else: # Repbase annotation
+		repeat = classes.PierRepeat(repeatName)
+	print repeatName
 	return repeat
+
+if __name__ == '__main__':
+	main()
