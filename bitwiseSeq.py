a577b7e8241f62582fb7475e718a62d8606baf8d
Brian Lin <me@brianl.in>
2013-06-27 17:39:35 -0700

    experimental speedup for block conversion

--- bitwiseSeq.py
+++ bitwiseSeq.py
@@ -6,8 +6,7 @@ import math
 
 class BitwiseSeq(object):
 
-	def __init__(self, name, length):
-		self.name = name
+	def __init__(self, length):
 		self.length = length
 		self.bitint = 0
 		self.bitmax = pow(2, length) - 1
@@ -39,7 +38,7 @@ class BitwiseSeq(object):
 		return countFlippedBits(diffBitint)
 
 	def __str__(self):
-		return '{}: {:d}'.format(self.name, self.length)
+		return '{:d}:{:d}'.format(self.length, self.bitint)
 
 def countFlippedBits(bitint): #counts number of flipped bits in O(n), n = # of flipped bits
 		nflipped = 0
@@ -54,5 +53,14 @@ def convertIntToBitint(number):
 
 def convertBlockToBitint(block):
 	(start, end) = block
-	return sum(convertIntToBitint(base) for base in range(start, end))
+	bitstring = ''
+	for i in range(start - 1, end):
+		bitstring += '1'
+		i += 1
+	for i in range(0, start):
+		bitstring += '0'
+	return int(bitstring, 2)
 
+def convertBlockToBitint2(block):
+	(start, end) = block
+	return sum(convertIntToBitint(base) for base in range(start, end))
