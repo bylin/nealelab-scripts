342916983382cd06aaae0655a59d6a5d1dab328a
Brian Lin <me@brianl.in>
2013-07-01 10:20:59 -0700

    modify to account for bitwiseSeq change

--- RMStats.py
+++ RMStats.py
@@ -4,21 +4,22 @@
 # Need to sort .out file by S-W score before running!
 import sys, re, glob, classes, argparse, subprocess, pickle, bitwiseSeq
 from pickler import getFromPickleJar
+from Bio import SeqIO
 
 pickledRepbaseFile = 'repBaseDict.pkl'
-pickledRawSequenceFile = 'bitwiseRawSeqs.pkl'
+rawSeqFile = 'ptaeda.v0.9_bin001_of100.fsa'
 
 def main():
-	fileList = getFileList()
+	args = parseArgs()
+	fileList = getFileList(args)
 	stats = classes.RepeatStats()
 	for i, currentFile in zip(range(1, len(fileList)+1), fileList):
-		print 'Parsing ' + currentFile + '...'
+		sys.stderr.write('Parsing {} ...\n'.format(currentFile))
 		addStatsFromFile(stats, currentFile)
-		print currentFile + ' finished, ' + str(len(fileList) - i) + ' files remaining'
-	print stats
+		sys.stderr.write('{} finished, {} files remaining\n'.format(currentFile, str(len(fileList) - i)))
+	sys.stderr.write(str(stats))
 
-def getFileList():
-	args = parseArgs()
+def getFileList(args):
 	if args.file:
 		return [args.file]
 	elif args.directory:
@@ -31,24 +32,44 @@ def parseArgs():
 	group = argParser.add_mutually_exclusive_group(required=True)
 	group.add_argument('-f', '--file', help='Input file')
 	group.add_argument('-d', '--directory', help='Input directory')
-	return argParser.parse_args()
+	argParser.add_argument('-o', '--output_file', help='Output file')
+	argParser.add_argument('-e', '--error_file', help='Error file, status messages will also be posted in this file')
+	args = argParser.parse_args()
+	if args.output_file:
+		sys.stdout = open(args.output_file, 'w')
+	if args.error_file:
+		sys.stderr = open(args.error_file, 'w')
+	return args
 	
 
 def addStatsFromFile(stats, fileHandle):
-	gffFile = open(fileHandle)
+	infile = open(fileHandle)
 	# need to skip first 3 lines of GFF files
-	for x in range(0,3): gffFile.readline()
-	for line in gffFile:
-		stats += parseLineIntoRepeatTuple(line)
+	for x in range(0,3): infile.readline()
+	sortedFile = sorted(infile, reverse=True)
+	counter = 0
+	for line in sortedFile:
+		counter += 1
+		if x % 100 == 0:
+			sys.stdout.write('.')
+			sys.stdout.flush()
+		try:
+			repeatTuple = parseLineIntoRepeatTuple(line)
+			print '{} {}'.format(repeatTuple[0], repeatTuple[1])
+			stats += repeatTuple
+		except:
+			sys.stderr.write("Unable to parse line: {}".format(line))
 
 def parseLineIntoRepeatTuple(line):
-	tabs = re.findall('\W+', line)
-	repeatName = tabs[9]
-	rawSeqName = tabs[4]
-	block = (int(tabs[5]), int(tabs[6]))
+	try:
+		tabs = re.findall('\S+', line)
+		repeatName = tabs[9]
+		rawSeqName = tabs[4]
+		block = (int(tabs[5]), int(tabs[6]) + 1)
+	except:
+		raise
 	tracker = RawSeqTracker()
-	difference = tracker.addBlockReturnDifference(rawSeqName, block)
-	blockSize = block[1] - block[0] - difference
+	blockSize = tracker.addBlockReturnDifference(rawSeqName, block)
 	repeat = buildRepeatFromName(repeatName)
 	return (repeat, blockSize)
 
@@ -79,12 +100,19 @@ class RepbaseMatcher(object):
 	def match(self, repeatName):
 		return self.repbase[repeatName]
 
+def storeRawSeqs(rawSeqFile):
+	seqs = {}
+	for seq in SeqIO.parse(rawSeqFile, 'fasta'):
+		seqs[seq.description] = bitwiseSeq.BitwiseSeq(len(seq))
+	return seqs
+
 class RawSeqTracker(object):
-	rawSeqs = getFromPickleJar(pickledRawSequenceFile) # dict containing bitwise seqs
-	
+	rawSeqs = storeRawSeqs(rawSeqFile)
 	def addBlockReturnDifference(self, name, block):
-		rawSeqs[name] += block
-		return rawSeqs[name].findBlockDifference(block)
+		diff = self.rawSeqs[name].addBlockReturnDifference(block)
+		print name, block, diff
+		return diff
 
 if __name__ == '__main__':
+	sys.stderr.write('Raw sequences loaded!\n')
 	main()
