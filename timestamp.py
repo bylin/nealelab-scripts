7ef85c31a1a2ca72d985035b064e82b7394ebd60
Brian Lin <me@brianl.in>
2013-08-01 12:34:01 -0700

    add more formats

--- timestamp.py
+++ timestamp.py
@@ -1,4 +1,13 @@
 import datetime
 
 def timestamp():
-	return datetime.datetime.today().strftime("%a-%b-%d-%Y-%H:%M:%S")
+	return datetime.datetime.today().strftime("%a-%b-%d-%Y-%H-%M-%S")
+
+def short():
+	return datetime.datetime.today().strftime("%m.%d.%H%M")
+
+def date():
+	return datetime.datetime.today().strftime("%m.%d.%y")
+
+def time():
+	return datetime.datetime.today().strftime("%H-%M-%S")
