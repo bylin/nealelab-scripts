38dafbaf11dc53e1981369c6eae4e2326130ca5c
Brian Lin <me@brianl.in>
2013-08-23 10:41:08 -0700

    options

--- statParser.py
+++ statParser.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 import re
-import str
+import string
 import sys
-a = open(sys.argv[1], 'r')
+import glob
+import subprocess
+f = glob.glob(sys.argv[1])
+if len(f) > 1:
+	for a in f:
+		print '========={}=========='.format(a)
+		subprocess.call('statParser.py {}'.format(a), shell=True)
+	exit()
 
+a = open(sys.argv[1], 'r')
+flag = sys.argv[2]
 class rep(object):
 	def __init__(self, n, bp):
 		self.nCopies = n
@@ -53,10 +62,49 @@ while True:
 		tabs = line.split('\t')
 		total.nCopies += int(tabs[1])
 		total.length += int(tabs[2])
-print 'TOTAL\t{}'.format(total)
-for classif, dicts in zip(['classes', 'orders', 'supers', 'families'], [classes, orders, supers, families]):
-	print str.upper(classif)
-	current = sorted(dicts, key=lambda key:dicts[key], reverse=True)
-	for classif in current:
-		print '{}\t{}'.format(classif, dicts[classif])
+#print 'TOTAL\t{}'.format(total)
+#for classif, dicts in zip(['classes', 'orders', 'supers', 'families'], [classes, orders, supers, families]):
+#	print str.upper(classif)
+#	current = sorted(dicts, key=lambda key:dicts[key], reverse=True)
+#	for classif in current:
+#		print '{}\t{}'.format(classif, dicts[classif])
 
+if flag == 'stats':
+	li = ['Gypsy', 'Copia', 'LTR', 'DIRS', 'Penelope', 'LINE', 'I', 'TIR', "II", 'NoCat', 'PotentialHostGene', 'TOTAL']
+	try: print '{}'.format(supers['Gypsy'])
+	except: print '0\t0'
+	try: print '{}'.format(supers['Copia'])
+	except: print '0\t0'
+	try: print '{}'.format(orders['LTR'])
+	except: print '0\t0'
+	try: print '{}'.format(orders['DIRS'])
+	except: print '0\t0'
+	try: print '{}'.format(orders['Penelope'])
+	except: print '0\t0'
+	try: print '{}'.format(orders['LINE'])
+	except: print '0\t0'
+	try: print '{}'.format(orders['SINE'])
+	except: print '0\t0'
+	try: print '{}'.format(classes['I'])
+	except: print '0\t0'
+	try: print '{}'.format(orders['TIR'])
+	except: print '0\t0'
+	try: print '{}'.format(orders['Helitron'])
+	except: print '0\t0'
+	try: print '{}'.format(classes['II'])
+	except: print '0\t0'
+	try: print '{}'.format(classes['NoCat'])
+	except: print '0\t0'
+	try: print '{}'.format(classes['PotentialHostGene'])
+	except: print '0\t0'
+	print '{}'.format(total)
+elif flag == 'coverage':
+	for family in sorted(families, key=lambda key:families[key], reverse=True):
+		print '{}\t{}'.format(family, families[family].length)
+elif flag == 'copy':
+	for family in sorted(families, key=lambda key:families[key].nCopies, reverse=True):
+		#if i > 100: break
+		print '{}\t{}'.format(family, families[family].nCopies)
+elif flag == 'both':
+	for family in sorted(families, key=lambda key:families[key], reverse=True):
+		print '{}\t{}\t{}'.format(family, families[family].nCopies, families[family].length)
